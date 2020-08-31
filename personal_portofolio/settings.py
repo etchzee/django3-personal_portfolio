b467947dcffea6720a0eeb53708011051e4590a6
Mohamed Wagdy <mohamed.h.wagdy@gmail.com>
2020-08-31 14:49:15 +0200

    local settings added

--- personal_portofolio/settings.py
+++ personal_portofolio/settings.py
@@ -124,3 +124,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("no local settings found looks like you are in production")
